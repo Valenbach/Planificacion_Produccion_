13081f760c8550948ad01566c9c602f149b2b269
valen <valentinalbarchetta@gmail.com>
2024-10-28 20:49:08 -0300

    se actualizan pruebas

---
+++ Pruebas_archivo_json.py
@@ -0,0 +1,19 @@
+import json
+
+def Crear_Procesos():
+    proceso= {}
+    
+    bandera= True
+    
+    while bandera:
+        try:
+            proceso["Molecula"]=int(input("ingrese el nombr e de la molecula\n"))
+            proceso["Etapas expansivas"]=int(input("ingrese la cantidad de etapas expansivas"))
+            bandera= False
+        except ValueError:
+            print("Datos invalidos, reintente la carga")
+    return proceso
+
+
+def Cargar_Procesos(Procesos_Guardados.json, ):
+    
